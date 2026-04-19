151b9695390dfbfb8a4de9c95f0e129d93af93fd
hesham898 <39433029+hesham898@users.noreply.github.com>
2026-04-19 05:08:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const http = require('http');
 
+http.createServer((req, res) => res.end('Bot is running')).listen(8080);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+client.on('ready', () => {
+    console.log(`Bot ${client.user.tag} is Online!`);
+    setInterval(() => {
+        const guild = client.guilds.cache.first();
+        if (guild) {
+            joinVoiceChannel({
+                channelId: '1495236948674215998',
+                guildId: guild.id,
+                adapterCreator: guild.voiceAdapterCreator,
+                selfMute: true,
+                selfDeaf: true
+            });
+        }
+    }, 30000); 
+});
+
+client.login('MTQ5NTI0MDE5NDMwNjM0MzAwNQ.G6_lBo.rybhD7sweE_jlipPCLLlQ4eJriJRYpn5GnSUkM');
